f4c9d7af9d62d6c4923648d9a54e593605c3386b
marcelooliveira <mclricardo@gmail.com>
2025-08-26 23:21:26 -0300

    ci cd 23:21

--- FunctionApp1/FunctionApp1/Function1.cs
+++ FunctionApp1/FunctionApp1/Function1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
 namespace FunctionApp1;
@@ -15,6 +16,7 @@ public class Function1
         _logger = logger;
     }
 
+    [FunctionName(nameof(Function1))]
     [Function(nameof(Function1))]
     public async Task Run(
         [ServiceBusTrigger("myqueue", Connection = "ServiceBusConnectionString")]
